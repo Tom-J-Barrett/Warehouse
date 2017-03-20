54bd72bea8a43825cf7046e26130f66365f719b5
michaelhasty <14171953@studentmail.ul.ie>
2017-03-20 14:01:47 +0000

    Initial Database Class Upload

--- src/IntelligentWarehouse.java
+++ src/IntelligentWarehouse.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
+import Database.Database;
 import control.AddNewProduct;
 import control.GenerateReport;
 import control.LogIn;
+import control.ReceiveInventory;
 import control.Ship;
 import control.Scrap;
 
@@ -11,13 +13,15 @@ public class IntelligentWarehouse {
 	public static void main(String[] args) {
 		boolean run=true;
 		//while(run){
+			Database db = new Database();
 			System.out.println("What would you like to do?:");
 			System.out.println("1.Generate Report");
 			System.out.println("2.Ship");
 			System.out.println("3.Login");  // <--- should be the first option no ?
 			System.out.println("4.Add New Product");
-			System.out.println("5.Scrap");
-			System.out.println("6.Exit");
+			System.out.println("5.Add Item to Inventory");
+			System.out.println("6.Scrap");
+			System.out.println("7.Exit");
 			
 			Scanner in=new Scanner(System.in);
 			int choice=Integer.parseInt(in.nextLine());
@@ -27,8 +31,9 @@ public class IntelligentWarehouse {
 			case 2:Ship ship=new Ship();break;
 			case 3:LogIn login= new LogIn();break;
 			case 4:AddNewProduct product=new AddNewProduct();break;
-			case 5:Scrap scrap=new Scrap();break;
-			case 6:run=false;break;
+			case 5:ReceiveInventory recInv=new ReceiveInventory(db);break;
+			case 6:Scrap scrap=new Scrap();break;
+			case 7:run=false;break;
 			}
 			
 			in.close();
